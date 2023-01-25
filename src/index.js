09f77f9e08dd41a5956a43e242c60c39f53d384f
AntonBabenko96 <ababenko159@gmail.com>
2023-01-25 15:34:10 +0200

    mon

--- src/index.js
+++ src/index.js
@@ -7,52 +7,63 @@ import "simplelightbox/dist/simple-lightbox.min.css";
 const searchForm = document.querySelector(".search-form");
 const searchInput = document.querySelector(".search-form input[name='searchQuery']");
 const loadMore = document.querySelector(".load-more");
-
 let valueSave = '';
 let pageNumber = 1;
+let numCard;
 loadMore.style.display = 'none'
-
 function onBtnSearch(e) {
     e.preventDefault();
+    numCard = 0;
+    pageNumber = 1;
     const inputValue = searchInput.value;
        if (inputValue.trim() !== '' && inputValue !== valueSave) {
-        gallery.innerHTML = ''
-        getPhoto(inputValue, pageNumber).then(foundData => {
+           getPhoto(inputValue, pageNumber).then(foundData => {
+            gallery.innerHTML = ''
             if (foundData.data.hits.length === 0) {
                 Notiflix.Notify.failure("Sorry, there are no images matching your search query. Please try again.");
-            } else {
+            }
+            else if (foundData.data.totalHits <= 40) {
+                makeMurkup(foundData.data.hits);
+                Notiflix.Notify.success(`"Hooray! We found ${foundData.data.totalHits} images."`);
+                numCard += 40;
+                valueSave = inputValue
+            }
+            else {
                 makeMurkup(foundData.data.hits);
                 Notiflix.Notify.success(`"Hooray! We found ${foundData.data.totalHits} images."`);
                 loadMore.style.display = 'flex'
+                numCard += 40;
+                valueSave = inputValue
             }
             // console.log(valueSave)
         })
-           valueSave = inputValue
+           
     } else {
         Notiflix.Notify.failure('The search field is not filled');
     }
     
 };
-let numCard = 40;
+
 function btnLoadMore(e) {
     e.preventDefault;
     const inputValue = searchInput.value;
     pageNumber++
    
-    if (inputValue !== '') {
-        getPhoto(inputValue, pageNumber).then(foundData => {
-            if (numCard <= foundData.data.totalHits) {
+    if (valueSave !== '') {
+        getPhoto(valueSave, pageNumber).then(foundData => {
+            if (numCard < foundData.data.totalHits) {
                  makeMurkup(foundData.data.hits);
                  Notiflix.Notify.success(`Add new photo`);
-                 loadMore.style.display = 'flex'
+                //  loadMore.style.display = 'flex'
                 numCard += 40;
-                // console.log(numCard)
-            } else {
+                
+            } else
+                makeMurkup(foundData.data.hits);
                 Notiflix.Notify.failure("We're sorry, but you've reached the end of search results.");
                 loadMore.style.display = 'none';
 
-            }
         })
+       
     } 
     
 };
